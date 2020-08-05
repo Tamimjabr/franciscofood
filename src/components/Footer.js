fcea935e0e0b1baea5c58c27dfeb1e28fc181106
T.J <Tamimjabr@github.com>
2020-08-05 21:24:20 +0200

    adding paypal payments

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,6 +8,13 @@ function Footer() {
     return(
       <div className="contact">
 
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top" id="donate">
+<input type="hidden" name="cmd" value="_s-xclick" />
+<input type="hidden" name="hosted_button_id" value="G9KLDPD4B5NXW" />
+<input type="image" src="https://www.paypalobjects.com/en_US/SE/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+<img alt="" border="0" src="https://www.paypal.com/en_SE/i/scr/pixel.gif" width="5" height="5" />
+</form>
+
       <div className="information">
         <h2>تواصلوا معنا<br/>
          فرانسيسكو فود<br/>
@@ -17,6 +24,7 @@ function Footer() {
     <a href="https://www.instagram.com/franciscofood/?igshid=1miprswt8m14f&fbclid=IwAR0LZGZoM_XVEUkfDLfZcLS2f7dZ1yrcjZOo1RZY9UQepX0ZtEObVoDxStQ"> <GrInstagram className="socialIcon instagram"/></a>
     <a href="mailto:FranciscoFood@hotmail.com?subject= اطلب وصفة"> <GrMail className="socialIcon envelope"/></a>      
       </div>
+
 
     </div>
     ) 
